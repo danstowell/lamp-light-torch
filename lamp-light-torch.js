42d76b253f9078c91ffbe1421b054f29b3576631
Dan Stowell <danstowell@users.sourceforge.net>
2013-11-23 13:25:40 +0000

    packaged version does not need to use installer api

--- lamp-light-torch.js
+++ lamp-light-torch.js
@@ -1,5 +1,4 @@
 var colmode = 0;
-var manifestLocation = "http://danstowell.github.io/lamp-light-torch/manifest.webapp";
 
 function flip(){
 	colmode = 1 - colmode;
@@ -12,26 +11,10 @@ function flip(){
 	document.body.style.color = thecolor;
 }
 
-function install(){
-	var installRequest = navigator.mozApps.install(manifestLocation);
-	installRequest.onsuccess = function(data) {
-		document.getElementById("installtext").style.color = 'inherit';
-	};
-	installRequest.onerror = function(err) {
-		console.log("Install error! " + manifestLocation);
-	};
-}
 
 
 /////////////////////////////////////////////
 window.onload = function(e) {
 	document.onclick = flip;
-	var request = navigator.mozApps.checkInstalled(manifestLocation);
-	request.onsuccess = function() {
-		if (!request.result) {
-			window.document.getElementById("installtext").onclick = install;
-			window.document.getElementById("installtext").style.color = 'gray';
-		}
-	};
 };
 
